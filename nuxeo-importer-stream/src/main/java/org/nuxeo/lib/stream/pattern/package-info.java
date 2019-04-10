a4190ecfb6769b754077e8a6d1631d938b6d68b8
Florent Guillaume <fguillaume@nuxeo.com>
2019-04-10 18:56:17 +0200

    NXP-25092: repository merged into nuxeo/nuxeo

--- nuxeo-importer-stream/src/main/java/org/nuxeo/lib/stream/pattern/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Here is a producer consumer pattern that relies on Log. Producer and Consumer communicate using
- * {@link org.nuxeo.lib.stream.pattern.Message}.
- */
-package org.nuxeo.lib.stream.pattern;
